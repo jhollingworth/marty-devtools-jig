e18978b459f74a01d27f47e363da0e2a45fe4b76
James Hollingworth <jamiehollingworth@gmail.com>
2015-01-15 08:06:45 +0000

    Default to multiple stores action

--- app/constants/testConstants.js
+++ app/constants/testConstants.js
@@ -1,6 +1,7 @@
 var Marty = require('marty');
 
 var TestConstants = Marty.createConstants([
+  'MULTIPLE_STORES',
   'NO_DISPATCH',
   'PROMISE_THEN_DISPATCH',
   'DISPATCH_THEN_PROMISE',
@@ -8,7 +9,6 @@ var TestConstants = Marty.createConstants([
   'PROMISE_FAIL',
   'NO_STORES',
   'ONE_STORE',
-  'MULTIPLE_STORES',
   'SINGLE_COMPONENT',
   'MULTIPLE_COMPONENTS',
   'FAIL_IN_ACTION_HANDLER',
